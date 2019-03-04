2af2ebc78f05138504081a3478aeb102861a402c
pdxdc <48166493+pdxdc@users.noreply.github.com>
2019-03-04 02:10:13 -0800

    Add files via upload
    It appears to be a working quickhull.  It may be needed to troubleshot a little more, but appears to work for the data tested.

---
+++ quickhull.py
@@ -0,0 +1,246 @@
+class Point:
+  def __init__(self, x, y):
+    self.x, self.y = x, y
+
+  def __str__(self):
+    return "{}, {}".format(self.x, self.y)
+
+  def __neg__(self):
+    return Point(-self.x, -self.y)
+
+  def __add__(self, point):
+    return Point(self.x+point.x, self.y+point.y)
+
+  def __sub__(self, point):
+    return self + -point
+
+
+
+a = Point(6,3)
+b = Point(7,38)
+c = Point(8,17)
+d = Point(10,5)
+e = Point(13,1)
+f = Point(17,4)
+g = Point(18,1)
+h = Point(21,5)
+i = Point(27,4)
+j = Point(28,5)
+k = Point(22,50)
+l = Point(28,10)
+m = Point(38,17)
+n = Point(48,51)
+o = Point(38,6)
+p = Point(23,0)
+q = Point(50,7)
+
+point_list_ = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q]
+
+def quickhull(point_list):
+
+	length = len(point_list)
+	left = [point_list[0]]
+	right = [point_list[0]]
+#	points = [point_list[0]]
+	for i in point_list[1:length-1]:
+		determinant = point_list[0].x * point_list[length-1].y + i.x * point_list[0].y \
+		+ point_list[length-1].x * i.y - i.x * point_list[length-1].y \
+		- point_list[length-1].x * point_list[0].y - point_list[0].x * i.y
+#		print determinant, "\n"
+
+		if determinant > 0:
+			left += [i]
+#			print "left: ", i, "\n"
+		if determinant < 0:
+			right += [i]
+#			print "right: ", i, "\n"
+
+	left += [point_list[length-1]]
+	right += [point_list[length-1]]
+#	print left
+#	print right
+	print "\n\nleft:"
+	for i in range( len(left)): 
+		print left[i].x, ",", left[i].y
+	print "\nright:"
+	for i in range( len(right)):
+		print right[i].x, ",", right[i].y
+	
+	points = [point_list[0]]
+	points += quickhull_upper(left)
+	print "returned from upper hull with these points:"
+	for p in points:
+		print p.x,",",p.y	
+	points += quickhull_lower(right)
+	points += [point_list[length-1]]
+	print "\nconvex set:"	
+	for p in points:
+		print p.x,",",p.y	
+	
+#	print points	
+#		print i.x
+#	for i in point_list:
+#		print "x = ", i.x, "  y = ", i.y
+	
+	return points
+
+def quickhull_upper(point_list):
+
+	length = len(point_list)
+	if length  == 3:
+		return [point_list[1]]
+	if length < 3:
+		return []
+
+	left = [point_list[0]]
+	max_det = 0
+	points = []
+
+	for i in point_list[1:length-1]:
+		determinant = point_list[0].x * point_list[length-1].y + i.x * point_list[0].y \
+		+ point_list[length-1].x * i.y - i.x * point_list[length-1].y \
+		- point_list[length-1].x * point_list[0].y - point_list[0].x * i.y
+#		print determinant, "\n"
+		if determinant > max_det:
+			convex_point = i
+			max_det = determinant
+#		if determinant > 0:
+#			left += [i]
+#			print "left: ", i, "\n"
+#		if determinant < 0:
+#			right += [i]
+#			print "right: ", i, "\n"
+
+	if max_det == 0:
+		return []
+
+	if max_det > 0:
+
+		right = [convex_point]
+
+		for i in point_list[1:length-1]:
+			determinant = point_list[0].x * convex_point.y + i.x * point_list[0].y \
+			+ convex_point.x * i.y - i.x * convex_point.y \
+			- convex_point.x * point_list[0].y - point_list[0].x * i.y
+#		print determinant, "\n"
+#		if determinant > max_det:
+#			convex_point = i
+#			max_det = determinant
+			if determinant > 0:
+				left += [i]
+#			print "left: ", i, "\n"
+
+		for i in point_list[1:length-1]:
+			determinant = convex_point.x * point_list[length-1].y + i.x * convex_point.y \
+			+ point_list[length-1].x * i.y - i.x * point_list[length-1].y \
+			- point_list[length-1].x * convex_point.y - convex_point.x * i.y
+#		print determinant, "\n"
+			if determinant > 0:
+				right += [i]
+
+	left += [convex_point]
+	right += [point_list[length-1]]
+#	print left
+#	print right
+	print "\n\nconvex_point_upper: ", convex_point.x,",",convex_point.y
+	print "\nleft:"
+	for i in range( len(left)): 
+		print left[i].x, ",", left[i].y
+	print "\nright:"
+	for i in range( len(right)):
+		print right[i].x, ",", right[i].y
+
+	if max_det > 0:
+		points = [convex_point]
+	points +=  quickhull_upper(left)
+	points +=  quickhull_upper(right)	
+
+
+		
+#		print i.x
+#	for i in point_list:
+#		print "x = ", i.x, "  y = ", i.y
+	
+	return points
+	
+def quickhull_lower(point_list):
+
+	length = len(point_list)
+	if length  == 3:
+		return [point_list[1]]
+	if length < 3:
+		return []
+
+	left = [point_list[0]]
+	max_det = 0
+	points = []
+
+	for i in point_list[1:length-1]:
+		determinant = point_list[0].x * point_list[length-1].y + i.x * point_list[0].y \
+		+ point_list[length-1].x * i.y - i.x * point_list[length-1].y \
+		- point_list[length-1].x * point_list[0].y - point_list[0].x * i.y
+#		print determinant, "\n"
+		if determinant < max_det:
+			convex_point = i
+			max_det = determinant
+#		if determinant > 0:
+#			left += [i]
+#			print "left: ", i, "\n"
+#		if determinant < 0:
+#			right += [i]
+#			print "right: ", i, "\n"
+
+	if max_det == 0:
+		return []
+
+	if max_det < 0:
+		right = [convex_point]
+
+		for i in point_list[1:length-1]:
+			determinant = point_list[0].x * convex_point.y + i.x * point_list[0].y \
+			+ convex_point.x * i.y - i.x * convex_point.y \
+			- convex_point.x * point_list[0].y - point_list[0].x * i.y
+#		print determinant, "\n"
+#		if determinant > max_det:
+#			convex_point = i
+#			max_det = determinant
+			if determinant < 0:
+				left += [i]
+#			print "left: ", i, "\n"
+
+		for i in point_list[1:length-1]:
+			determinant = convex_point.x * point_list[length-1].y + i.x * convex_point.y \
+			+ point_list[length-1].x * i.y - i.x * point_list[length-1].y \
+			- point_list[length-1].x * convex_point.y - convex_point.x * i.y
+#		print determinant, "\n"
+			if determinant < 0:
+				right += [i]
+
+	left += [convex_point]
+	right += [point_list[length-1]]
+#	print left
+#	print right
+	print "\n\nconvex_point_lower: ", convex_point.x,",",convex_point.y
+	print "\nleft:"
+	for i in range( len(left)): 
+		print left[i].x, ",", left[i].y
+	print "\nright:"
+	for i in range( len(right)):
+		print right[i].x, ",", right[i].y
+
+	if max_det < 0:
+		points = [convex_point]
+	points +=  quickhull_upper(left)
+	points +=  quickhull_upper(right)	
+
+		
+#		print i.x
+#	for i in point_list:
+#		print "x = ", i.x, "  y = ", i.y
+	
+	return points
+	
+quickhull(point_list_)
+#print c.x
+#print a.y
+#print b+b
