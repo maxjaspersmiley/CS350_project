187111cffb5bfcffc4ade89d088f7260e745f050
dimadotspace <46702603+dimadotspace@users.noreply.github.com>
2019-03-08 21:26:52 -0800

    Add files via upload
    Fixed bugs

--- bruteforce.py
+++ bruteforce.py
@@ -1,9 +1,8 @@
-#this is an example of how to build a cartesian list with point
-#class and then pass them in to functions to be processed
 from point_class import Point 
 
-point_list = []
-point_list.extend([Point(1,1),Point(10,10),Point(1,10),Point(10,1),Point(5,5)])
+
+def sideOf(x1, y1, x2, y2, x3, y3): 
+    return (x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)
 
 def bruteforce(point_list):
 
@@ -16,7 +15,6 @@ def bruteforce(point_list):
         y1 = P1.y
                
         for j in range(0, len(point_list)):
-            
             if (i !=j):
  
                 P2 = point_list[j]
@@ -26,27 +24,46 @@ def bruteforce(point_list):
                 sameSide = True
 
                 for k in range(0, len(point_list)):
-            
                     if(j != k) and (i != k):
    
                         P3 = point_list[k]
                         x3 = P3.x
                         y3 = P3.y
-                        
-                        #formula derived from https://math.stackexchange.com/questions/757591/how-to-determine-the-side-on-which-a-point-lies
-                        d = (x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)
+     
+                        d = sideOf(x1, y1, x2, y2, x3, y3)
                     
                         if (d > 0):
                             sameSide = False
                             break
 
                 if(sameSide):
-                    
-                    #print("[",x1,",",y1,"],[",x2,",",y2,"]")
 
-                    convex_hull.append([P1,P2])
+                    convex_hull.append(P1)
+                    convex_hull.append(P2)
+
 
     return convex_hull
 
 if __name__ == "__main__":
-    bruteforce(point_list)
+
+    point_list = []
+
+    upper_limit = 100
+
+
+    #create a box from 1,1 to 30,30
+    point_list.append(Point(1,1))
+    point_list.append(Point(1,upper_limit))
+    point_list.append(Point(upper_limit,1))
+    point_list.append(Point(upper_limit, upper_limit))
+
+
+    #fill in that box with points
+    for i in range(2,upper_limit -1):
+        for j in range(2,upper_limit-1):
+            point_list.append(Point(i,j))
+
+    convex_hull = (bruteforce(point_list))
+
+    for point in convex_hull:
+        print(point.x, ", ", point.y)
