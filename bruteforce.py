21f44166825b86ec42ed040839bed15abc432e4e
Max Smiley <max.jasper.smiley@gmail.com>
2019-03-10 15:19:19 -0700

    cmt

--- bruteforce.py
+++ bruteforce.py
@@ -32,7 +32,7 @@ def bruteforce(point_list):
 
                         d = sideOf(x1, y1, x2, y2, x3, y3)
 
-                        if (d > 0):
+                        if (d < 0):
                             sameSide = False
                             break
 
